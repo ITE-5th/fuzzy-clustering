05de11d72e12a86ab630abe606b9487d700e830c
Obada Jabassini <obada.jabassini@gmail.com>
2018-04-13 20:29:26 +0300

    Running(but not working :))

--- fuzzy_cmean.py
+++ fuzzy_cmean.py
@@ -3,45 +3,39 @@ from scipy.linalg import norm
 
 
 class FuzzyCMeans:
-    def __init__(self, number_of_clusters: int, max_iterations: int = 1000, epsilon: float = 0.1):
+    def __init__(self, number_of_clusters: int, iterations: int = 1000, m: int = 2):
         self.number_of_clusters = number_of_clusters
-        self.max_iterations = max_iterations
-        self.epsilon = epsilon
+        self.iterations = iterations
         self.u = self.centers = None
+        self.m = m
 
     def fit(self, X):
         rows, cols = X.shape
-        u = np.random.randn(rows, self.number_of_clusters)
-        new_u = np.full((rows, self.number_of_clusters), 2)
+        u = np.random.uniform(0, 1, (rows, self.number_of_clusters))
         iteration = 1
-        while iteration <= self.max_iterations and self.stop_condition(u, new_u):
+        while iteration <= self.iterations:
             centers = self.compute_next_centers(X, u)
-            u = new_u
-            new_u = self.compute_next_u(X, centers)
+            u = self.compute_next_u(X, centers)
             iteration += 1
-        self.u = u
-        self.centers = centers
+        return centers, u
 
     def compute_next_centers(self, X, u):
         centers = []
         for i in range(self.number_of_clusters):
             temp = np.sum(u[:, i].reshape(-1, 1) * X, axis=0) / np.sum(u[:, i]).reshape(-1)
+            temp = np.fmax(temp, np.finfo(np.float64).eps)
             centers.append(temp)
         centers = np.asarray(centers)
-        print("centers:")
-        print(centers)
         return centers
 
     def compute_next_u(self, X, centers):
-        new_u = np.zeros((X.shape[0], self.number_of_clusters))
+        u = np.zeros((X.shape[0], self.number_of_clusters))
         for i in range(X.shape[0]):
             for j in range(self.number_of_clusters):
                 temp = X[i, :].reshape(1, -1)
                 t = centers[j, :].reshape(1, -1)
                 neom = norm(temp - t)
                 denom = norm(temp - centers)
-                new_u[i, j] = 1 / np.power(neom / denom, 2).sum()
-        return new_u
-
-    def stop_condition(self, u, new_u):
-        return abs(u.max() - new_u.max()) > self.epsilon
+                u[i, j] = 1 / np.power(neom / denom, 2).sum()
+        u = np.fmax(u, np.finfo(np.float64).eps)
+        return u
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ sns.set()
 X, _ = make_blobs(n_samples=300, centers=3,
                   cluster_std=0.60, random_state=0)
 fuzzy_cmean = FuzzyCMeans(number_of_clusters=3)
-fuzzy_cmean.fit(X)
-centers, u = fuzzy_cmean.centers, fuzzy_cmean.u
-plt.scatter(X[:, 0], X[:, 1], s=50)
+centers, u = fuzzy_cmean.fit(X)
+print(centers)
+plt.scatter(X[:, 0], X[:, 1], s=50, cmap='viridis')
+plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5)
 plt.show()
