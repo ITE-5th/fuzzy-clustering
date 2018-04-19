06dedc1e6b236de92d68329f23e58bc4b4c33aeb
anjd <ite.anjd@outlook.com>
2018-04-19 05:55:56 +0300

    u initialization

--- cmeans.py
+++ cmeans.py
@@ -5,7 +5,7 @@ from scipy.linalg import norm
 class CMeans:
     def __init__(self):
         super().__init__()
-        self.u, self.centers = None, None
+        self.u, self.centers, self.m = None, None, None
         self.clusters_count = None
 
     def fit(self, X, m, clusters_count, iterations=100):
@@ -14,12 +14,12 @@ class CMeans:
         C = self.clusters_count = clusters_count
         centers = []
 
-        u = np.random.randn(N, C)
+        u = np.random.dirichlet(np.ones(C), size=N)
 
         iteration = 0
         while iteration < iterations:
             centers = self.next_centers(X, u, m)
-            u = self.next_u(X, centers, m)
+            u = self.next_u(X, centers)
             iteration += 1
 
         self.u = u
@@ -28,30 +28,24 @@ class CMeans:
 
     def next_centers(self, X, u, m):
         clusters = []
+
         for j in range(self.clusters_count):
-            nominator = 0
+            numerator = 0
             for i in range(X.shape[0]):
-                nominator += (u[i, j] ** m) * X[i]
+                numerator += (u[i, j] ** m) * X[i]
 
             denominator = np.sum(u[:, j] ** m)
-            clusters.append(nominator / denominator)
+            clusters.append(numerator / denominator)
 
         return np.asarray(clusters)
 
-    def next_u(self, X, centers, m):
+    def next_u(self, X, centers):
         N = X.shape[0]
         C = self.clusters_count
         new_u = np.zeros((N, C))
 
-        power = float(2 / (m - 1))
         for i in range(N):
-            # for j in range(C):
-            #     nominator_ = norm(X[i, :] - centers[j, :])
-            #     denominator_ = norm(X[i, :] - centers, axis=1)
-            #     denominator = (nominator_ / denominator_) ** power
-            #     new_u[i, j] = 1 / (denominator + 1e-7).sum()
-            predict = self._predict(X[i], centers)
-            new_u[i] = predict
+            new_u[i] = self._predict(X[i], centers)
         return new_u
 
     def _predict(self, X, centers):
@@ -59,10 +53,10 @@ class CMeans:
         u = np.zeros(self.clusters_count)
 
         denominator_ = norm(X - centers, axis=1)
-        for j in range(self.clusters_count):
-            nominator_ = norm(X - centers[j, :])
-            denominator = (nominator_ / denominator_) ** power
-            u[j] = 1 / (denominator + 1e-7).sum()
+        for k in range(self.clusters_count):
+            numerator_ = norm(X - centers[k])
+            denominator = np.power((numerator_ / denominator_), power)
+            u[k] = 1 / denominator.sum()
 
         return u
 
