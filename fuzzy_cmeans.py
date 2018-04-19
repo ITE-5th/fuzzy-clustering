37ccc46e2a78e73b9c0e45597aa21ffdf028c79a
Obada Jabassini <obada.jabassini@gmail.com>
2018-04-19 13:55:20 +0300

    Add some speed-ups

--- fuzzy_cmeans.py
+++ fuzzy_cmeans.py
@@ -1,61 +1,72 @@
-from functools import partial
-
 import numpy as np
 from scipy.linalg import norm
 
 
 class FuzzyCMeans:
-    def __init__(self, number_of_clusters: int, m: int = 2):
-        self.number_of_clusters = number_of_clusters
-        self.m = m
-        self.centers = None
-        self.u = None
+    def __init__(self):
+        super().__init__()
+        self.u, self.centers, self.m = None, None, None
+        self.clusters_count = None
 
-    def fit(self, X, iterations: int = 1000):
-        old_u = np.random.rand(X.shape[0], self.number_of_clusters)
-        old_u = np.fmax(old_u, np.finfo(np.float64).eps)
-        self.u = np.random.rand(X.shape[0], self.number_of_clusters)
-        self.u = np.fmax(self.u, np.finfo(np.float64).eps)
+    def fit(self, X, m, clusters_count, iterations=100):
+        N = X.shape[0]
+        self.m = m
+        C = self.clusters_count = clusters_count
+        centers = []
+
+        old_u = np.random.randn(N, clusters_count)
+        u = np.random.dirichlet(np.ones(C), size=N)
+        epsilon = 0.01
         iteration = 1
-        epsilon = 0.1
-        while iteration <= iterations and np.abs(self.u - old_u).sum() > epsilon:
-            self.centers = self.compute_centers(X)
-            old_u = self.u
-            self.u = self.compute_u(X)
+        while iteration <= iterations and np.abs(u - old_u).sum() > epsilon:
+            centers = self.next_centers(X, u, m)
+            old_u = u
+            u = self.next_u(X, centers)
             iteration += 1
 
-    def predict(self, X):
-        return np.apply_along_axis(self.compute_u_for_row, axis=1, arr=X)
+        self.u = u
+        self.centers = centers
+        return centers
 
-    def compute_centers(self, X: np.ndarray):
-        fun = partial(self.calculate_multiply, arr=X)
-        neo = np.apply_along_axis(fun, 0, self.u)
-        dem = self.u.sum(axis=0)
-        result = neo / dem
-        result = np.fmax(result, np.finfo(np.float64).eps)
-        return result
+    def next_centers(self, X, u, m):
+        clusters = []
 
-    def compute_u(self, X):
-        u = np.zeros((X.shape[0], self.number_of_clusters))
-        for i in range(X.shape[0]):
-            u[i] = self.compute_u_for_row(X[i])
-        u = np.fmax(u, np.finfo(np.float64).eps)
+        for j in range(self.clusters_count):
+            numerator = 0
+            for i in range(X.shape[0]):
+                numerator += (u[i, j] ** m) * X[i]
+
+            denominator = np.sum(u[:, j] ** m)
+            clusters.append(numerator / denominator)
+
+        return np.asarray(clusters)
+
+    def next_u(self, X, centers):
+        N = X.shape[0]
+        C = self.clusters_count
+        new_u = np.zeros((N, C))
+
+        for i in range(N):
+            new_u[i] = self._predict(X[i], centers)
+        return new_u
+
+    def _predict(self, X, centers):
+        power = float(2 / (self.m - 1))
+        u = np.zeros(self.clusters_count)
+
+        denominator_ = norm(X - centers, axis=1)
+        for k in range(self.clusters_count):
+            numerator_ = norm(X - centers[k])
+            denominator = np.power((numerator_ / denominator_), power)
+            u[k] = 1 / denominator.sum()
+
         return u
 
-    def compute_u_for_row(self, row):
-        row = row.reshape(1, -1)
-        temp = (row - self.centers)
-        temp = norm(temp, axis=1)
-        temp = temp.reshape(1, -1)
-        temp = np.power(temp, 2 / (self.m - 1))
-        t = temp.sum()
-        result = temp / t
-        result = 1 / result
-        result = np.fmax(result, np.finfo(np.float64).eps)
-        return result.reshape(-1)
+    def predict(self, X):
+        if len(X.shape) == 1:
+            X = np.expand_dims(X, axis=0)
 
-    @staticmethod
-    def calculate_multiply(col, arr):
-        col = col.reshape(arr.shape[0], -1)
-        temp = np.sum(col * arr, axis=0)
-        return temp
+        u = []
+        for i in range(len(X)):
+            u.append(self._predict(X[i], self.centers))
+        return np.argmax(u, axis=1)
--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import random
+
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from skfuzzy.cluster import cmeans
 from sklearn.datasets.samples_generator import make_blobs
 
 from fuzzy_cmeans import FuzzyCMeans
 
 sns.set()
 number_of_clusters = 2
-X, _ = make_blobs(n_samples=300, centers=number_of_clusters,
-                  cluster_std=0.60, random_state=0)
-fuzzy_cmeans = FuzzyCMeans(number_of_clusters=number_of_clusters)
-fuzzy_cmeans.fit(X, iterations=100)
-centers, u = fuzzy_cmeans.centers, fuzzy_cmean.u
+m = 2
+iterations = 2000
+X, _ = make_blobs(n_samples=200, centers=number_of_clusters,
+                  cluster_std=0.60, random_state=random.randint(0, 200))
+fuzzy_cmeans = FuzzyCMeans()
+fuzzy_cmeans.fit(X, m, number_of_clusters, iterations=iterations)
+centers, u = fuzzy_cmeans.centers, fuzzy_cmeans.u
 print("centers:")
 print(centers)
 plt.scatter(X[:, 0], X[:, 1], s=50, cmap='viridis')
