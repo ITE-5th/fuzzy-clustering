5066b8fe4dba62c3ca8d1b1eb24c8268378d0052
anjd <ite.anjd@outlook.com>
2018-05-01 14:36:26 +0300

    speed it up

--- fcm.py
+++ fcm.py
@@ -3,12 +3,13 @@ from scipy.linalg import norm
 
 
 class FCM:
-    def __init__(self, n_cluster=4, max_iter=100, m=2):
+    def __init__(self, n_cluster=4, max_iter=100, m=2, error=1e-6):
         super().__init__()
         self.u, self.centers, self.m = None, None, None
         self.clusters_count = n_cluster
         self.max_iter = max_iter
         self.m = m
+        self.error = error
 
     def fit(self, X):
         N = X.shape[0]
@@ -19,47 +20,34 @@ class FCM:
 
         iteration = 0
         while iteration < self.max_iter:
+            u2 = u.copy()
+
             centers = self.next_centers(X, u)
             u = self.next_u(X, centers)
             iteration += 1
+
+            # Stopping rule
+            if norm(u - u2) < self.error:
+                break
 
         self.u = u
         self.centers = centers
         return centers
 
     def next_centers(self, X, u):
-        clusters = []
-
-        for j in range(self.clusters_count):
-            numerator = 0
-            for i in range(X.shape[0]):
-                numerator += (u[i, j] ** self.m) * X[i]
-
-            denominator = np.sum(u[:, j] ** self.m)
-            clusters.append(numerator / denominator)
-
-        return np.asarray(clusters)
+        um = u ** self.m
+        return (X.T @ um / np.sum(um, axis=0)).transpose()
 
     def next_u(self, X, centers):
-        N = X.shape[0]
-        C = self.clusters_count
-        new_u = np.zeros((N, C))
-
-        for i in range(N):
-            new_u[i] = self._predict(X[i], centers)
-        return new_u
+        return np.apply_along_axis(self._predict, 1, X, centers)
 
     def _predict(self, X, centers):
         power = float(2 / (self.m - 1))
-        u = np.zeros(self.clusters_count)
+        x1 = norm(X - centers, axis=1) ** power
+        denominator_ = x1.reshape((1, -1)).repeat(x1.shape[0], axis=0)
+        denominator_ = x1[:, None] / denominator_
 
-        denominator_ = norm(X - centers, axis=1)
-        for k in range(self.clusters_count):
-            numerator_ = norm(X - centers[k])
-            denominator = np.power((numerator_ / denominator_), power)
-            u[k] = 1 / denominator.sum()
-
-        return u
+        return 1 / denominator_.sum(1)
 
     def predict(self, X):
         if len(X.shape) == 1:
