6579d7e844603f586d65d69ce9d882f848a0dbb9
anjd <ite.anjd@outlook.com>
2018-05-03 01:41:55 +0300

    gk fully vectorized but predict

--- algorithms/gk.py
+++ algorithms/gk.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.linalg import det, inv, norm
+from scipy.linalg import norm
 
 
 class GK:
@@ -23,8 +23,8 @@ class GK:
             u2 = u.copy()
 
             centers = self.next_centers(z, u)
-            F = self._covariance(z, centers, u)
-            dist = self._distance(z, centers, F)
+            f = self._covariance(z, centers, u)
+            dist = self._distance(z, centers, f)
             u = self.next_u(dist)
             iteration += 1
 
@@ -38,67 +38,37 @@ class GK:
 
     def next_centers(self, z, u):
         um = u ** self.m
-        N = len(z)
-        C = self.clusters_count
-
-        v = []
-        for i in range(C):
-            numerator = 0
-            denominator = um[i].sum()
-
-            for k in range(N):
-                numerator += um[i, k] * z[k]
-
-            v.append(numerator / denominator)
-
-        return np.array(v)
-        # return X.T @ um.T / np.sum(um, axis=1)
+        return ((um @ z).T / um.sum(axis=1)).T
 
     def _covariance(self, z, v, u):
-        cov = []
-        N = len(z)
-        C = self.clusters_count
-
         um = u ** self.m
-        for i in range(C):
-            denominator = um[i].sum()
-            numerator = 0
-            for k in range(N):
-                # todo: v[:, i] or v[i, :]
-                d = np.expand_dims(z[k] - v[i], axis=1)
-                d = np.matmul(d, d.transpose())
-                numerator += um[i, k] * d
 
-            cov.append(numerator / denominator)
+        denominator = um.sum(axis=1).reshape(-1, 1, 1)
+        temp = np.expand_dims(z.reshape(z.shape[0], 1, -1) - v.reshape(1, v.shape[0], -1), axis=3)
+        temp = np.matmul(temp, temp.transpose((0, 1, 3, 2)))
+        numerator = um.transpose().reshape(um.shape[1], um.shape[0], 1, 1) * temp
+        numerator = numerator.sum(0)
 
-        return np.asarray(cov)
+        return numerator / denominator
 
-    def _distance(self, z, v, F):
-        C = self.clusters_count
-        N = len(z)
-        d = np.zeros((C, N))
-
-        for i in range(C):
-            for k in range(N):
-                temp = (z[k] - v[i]).reshape(-1, 1)
-                temp2 = np.power(det(F[i]), 1 / self.m) * inv(F[i])
-                temp2 = np.matmul(temp.transpose(), temp2)
-                d[i, k] = np.matmul(temp2, temp)
-
-        return np.fmax(d, 1e-8)
+    def _distance(self, z, v, f):
+        dif = np.expand_dims(z.reshape(z.shape[0], 1, -1) - v.reshape(1, v.shape[0], -1), axis=3)
+        determ = np.power(np.linalg.det(f), 1 / self.m)
+        det_time_inv = determ.reshape(-1, 1, 1) * np.linalg.inv(f)
+        temp = np.matmul(dif.transpose((0, 1, 3, 2)), det_time_inv)
+        output = np.matmul(temp, dif).squeeze().T
+        return np.fmax(output, 1e-8)
 
     def next_u(self, d):
         return self._predict(d)
 
     def _predict(self, d):
-        C, N = d.shape
+        d = d.transpose()
 
         power = float(1 / (self.m - 1))
-        u = np.zeros((C, N))
+        denominator_ = d.reshape((d.shape[0], 1, -1)).repeat(d.shape[-1], axis=1)
+        denominator_ = np.power(d[:, np.newaxis, :] / denominator_.transpose((0, 2, 1)), power)
+        denominator_ = 1 / denominator_.sum(1)
+        denominator_ = denominator_.transpose()
 
-        for i in range(C):
-            for k in range(N):
-                denominator = np.power((d[i, k] / d[:, k]), power)
-                u[i, k] = 1 / denominator.sum()
-
-        return u
+        return denominator_
--- tests/main.py
+++ tests/main.py
@@ -6,9 +6,10 @@ import seaborn as sns
 from sklearn.datasets.samples_generator import make_blobs
 
 from algorithms.fuzzy_cmeans import FuzzyCMeans
+from algorithms.gk import GK
 
 sns.set()
-number_of_clusters = 3
+number_of_clusters = 5
 m = 2
 iterations = 2000
 samples = 200
@@ -16,14 +17,12 @@ std = 1
 X, _ = make_blobs(n_samples=samples, centers=number_of_clusters,
                   cluster_std=std, random_state=random.randint(0, 200))
 fuzzy_cmeans = FuzzyCMeans()
-fuzzy_cmeans.fit(X, m, number_of_clusters, iterations=iterations)
-centers, u = fuzzy_cmeans.centers, fuzzy_cmeans.u
+gk = GK(number_of_clusters)
+gk.fit(X)
+centers = gk.centers
+
 print("centers:")
 print(centers)
 plt.scatter(X[:, 0], X[:, 1], s=50, cmap='viridis')
 plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.7)
 plt.show()
-point = np.array([[3, 1]])
-predicted = fuzzy_cmeans.predict(point)
-print()
-print(f"predicted = {predicted}")
